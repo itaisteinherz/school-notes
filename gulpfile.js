4c4f3e0c77c5f3d34c487bc0c90f2209539b1c56
Itai Steinherz <itaisteinherz@gmail.com>
2019-02-21 20:39:37 +0200

    Configure `deploy` npm script

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 const path = require("path");
 
+const dotenv = require("dotenv");
 const gulp = require("gulp");
 const del = require("del");
 const rename = require("gulp-rename");
 const insert = require("gulp-insert");
+
+dotenv.config();
 
 gulp.task("cleanup", () => {
 	return del(["src/pages/notes/**", "!src/pages/notes", "!src/pages/notes/Example/**"]);
 });
 
 gulp.task("fetch:md", async () => {
-	const notesDir = path.resolve(process.env.NOTES_SOURCE);
+	const notesDir = process.env.NOTES_SOURCE;
 
-	return gulp.src(`${notesDir}/**/*.md`)
+	return gulp.src(path.join(notesDir, "/**/*.md"))
 		.pipe(rename(path => {
 			path.dirname += `/${path.basename}`;
 			path.basename = "index";
