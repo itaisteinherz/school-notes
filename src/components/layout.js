4f213df642ef06e96935687f8b7e15cbf3bab0d7
Itai Steinherz <itaisteinherz@gmail.com>
2019-03-05 19:26:47 +0200

    Move to using `modern-normalize`

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Helmet from "react-helmet";
 import {StaticQuery, graphql} from "gatsby";
 
 import Header from "./header";
-import "normalize.css";
+import "modern-normalize"; // eslint-disable-line import/no-unassigned-import
 import "./layout.css";
 
 const Layout = ({children}) => (
